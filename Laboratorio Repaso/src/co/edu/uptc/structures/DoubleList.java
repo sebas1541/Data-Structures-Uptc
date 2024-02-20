92dfafecbe198d828607f8593934f7b7002853bd
Sebastian <sebastian.ca0102@gmail.com>
2024-02-19 23:12:13 -0500

    Removing unused imports

--- Laboratorio Repaso/src/co/edu/uptc/structures/DoubleList.java
+++ Laboratorio Repaso/src/co/edu/uptc/structures/DoubleList.java
@@ -1,7 +1,6 @@
 package co.edu.uptc.structures;
 
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
--- Laboratorio Repaso/src/co/edu/uptc/test/Test.java
+++ Laboratorio Repaso/src/co/edu/uptc/test/Test.java
@@ -2,6 +2,6 @@ package co.edu.uptc.test;
 
 public class Test {
     public static void main(String[] args) {
-
+        System.out.println("Hola mundo");
     }
 }
