8bde2abee9afbdda6f41f2d6555d1fb5c5e996f7
Sebastian <sebastian.ca0102@gmail.com>
2024-02-21 00:44:51 -0500

    Final Touches

--- Laboratorio Repaso/src/co/edu/uptc/presenters/Presenter.java
+++ Laboratorio Repaso/src/co/edu/uptc/presenters/Presenter.java
@@ -5,6 +5,8 @@ import co.edu.uptc.models.RetailStore;
 import co.edu.uptc.models.RetailStoreManager;
 import co.edu.uptc.views.View;
 
+import java.util.ArrayList;
+
 public class Presenter {
     private RetailStoreManager retailStoreManager;
     private View view;
@@ -14,6 +16,20 @@ public class Presenter {
         this.view = new View();
     }
 
+    public void createFixedStores(){
+        retailStoreManager.createStore("Exito Viva Tunja", "Calle X");
+        retailStoreManager.createStore("Exito Centro Tunja", "Calle Y");
+
+        Product product1 = new Product(2, "Manzana", 11, 800);
+        Product product2 = new Product(3, "Leche", 22,  5000);
+        Product product3 = new Product(10, "Cereal", 18, 4000);
+        Product product4 = new Product(6, "Pan", 15, 500);
+
+        retailStoreManager.findStoreByName("Exito Viva Tunja").addProduct(product1);
+        retailStoreManager.findStoreByName("Exito Centro Tunja").addProduct(product2);
+        retailStoreManager.findStoreByName("Exito Viva Tunja").addProduct(product3);
+        retailStoreManager.findStoreByName("Exito Centro Tunja").addProduct(product4);
+    }
 
 
     public void run() {
@@ -21,6 +37,7 @@ public class Presenter {
     }
 
     private void mainMenu() {
+        createFixedStores();
         int option;
         do {
             view.showMessage("Menú Principal\n1. Administrar Tiendas\n2. Ver Valor de la Cadena\n3. Salir");
@@ -44,7 +61,7 @@ public class Presenter {
     private void manageStores() {
         int option;
         do {
-            view.showMessage("Gestión de Tiendas\n1. Agregar Tienda\n2. Agregar Producto a Tienda\n3. Ver Productos de la Tienda\n4. Vender Artículo\n5. Regresar al Menú Principal");
+            view.showMessage("Gestión de Tiendas\n1. Agregar Tienda\n2. Agregar Producto a Tienda\n3. Ver Productos de la Tienda\n4. Vender Artículo\n5.Remover Articulos en rango\n6.Número de artículos\n7.Ver info de artículo con Id \n8. Regresar al Menú Principal");
             option = view.readInt("Elige una opción: ");
             switch (option) {
                 case 1:
@@ -63,14 +80,15 @@ public class Presenter {
                     removeRange();
                     break;
                 case 6:
-                    removeRange();
+                    numberOfArticles();
                     break;
                 case 7:
+                    viewProductInfoById();
                     break;
                 default:
                     view.showMessage("Intente de nuevo");
             }
-        } while (option != 5);
+        } while (option != 8);
     }
 
     private void createStore() {
@@ -79,6 +97,8 @@ public class Presenter {
         retailStoreManager.createStore(name, address);
         view.showMessage("¡Tienda agregada exitosamente!");
     }
+
+
 
 
     private void addProductToStore() {
@@ -111,16 +131,64 @@ public class Presenter {
         }
     }
 
+    /**
+     * Clase utilitaria para imprimor productos
+     * Se usan cadenas de formato dependiendo si es
+     * entero, string o double
+     * @param product Es el producto para formatear.
+     * @return un string de productos para implementar en la vista.
+     */
+    private String formatProductDetails(Product product) {
+        return String.format(
+                "Detalles de Producto:\n" +
+                        "-----------------\n" +
+                        "Código ID:    %d\n" +
+                        "Nombre:       %s\n" +
+                        "Cantidad:   %d\n" +
+                        "Precio:      $%.2f\n",
+                product.getIdCode(), product.getName(), product.getQuantity(), product.getPrice());
+    }
 
     private void viewStoreProducts() {
         String storeName = view.readString("Ingrese el nombre de la tienda: ");
         RetailStore store = retailStoreManager.findStoreByName(storeName);
         if (store != null) {
-            view.showMessage(store.loadProducts().toString());
+            ArrayList<Product> products = store.loadProducts();
+            StringBuilder productsDetails = new StringBuilder();
+            for (Product product : products) {
+                productsDetails.append(formatProductDetails(product)).append("\n");
+            }
+            view.showMessage(productsDetails.toString());
         } else {
             view.showMessage("Tienda no encontrada.");
         }
     }
+
+    private void viewProductInfoById(){
+        String storeName = view.readString("Ingrese el nombre de la tienda donde añadir el producto: ");
+        RetailStore store = retailStoreManager.findStoreByName(storeName);
+        if (store != null) {
+            int id = view.readInt("Ingrese el código del producto");
+            Product currentProduct = store.findById(id);
+            view.showMessage(formatProductDetails(currentProduct));
+        } else {
+            view.showMessage("Tienda no encontrada.");
+        }
+    }
+
+
+
+    private void numberOfArticles() {
+        String storeName = view.readString("Ingrese el nombre de la tienda: ");
+        RetailStore store = retailStoreManager.findStoreByName(storeName);
+        if (store != null) {
+            view.showMessage(String.valueOf(store.numberOfProducts()));
+        } else {
+            view.showMessage("Tienda no encontrada.");
+        }
+    }
+
+
 
 
     private void sellItem() {
