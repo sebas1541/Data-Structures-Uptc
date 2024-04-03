d77cf7f57f2b01a0b504eca1b0d5c60390878371
Sebastián <sebastian.ca0102@gmail.com>
2024-04-03 11:27:32 -0500

    Add MyBinaryTree

---
+++ MyBinaryTree/src/co/edu/uptc/structures/MyBinaryTree.java
@@ -0,0 +1,38 @@
+package co.edu.uptc.structures;
+
+import java.util.Comparator;
+
+public class MyBinaryTree<T> {
+    private Node<T> root;
+    private Comparator<T> comparator;
+
+    public MyBinaryTree(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(T data) {
+        root = insertRec(root, data);
+    }
+
+    private Node<T> insertRec(Node<T> current, T data) {
+
+        if (current == null) {
+            return new Node<>(data);
+        }
+
+        if (comparator.compare(data, current.getData()) < 0) {
+            current.setLeft(insertRec(current.getLeft(), data));
+        } else if (comparator.compare(data, current.getData()) > 0) {
+            current.setRight(insertRec(current.getRight(), data));
+        } else {
+
+            return current;
+        }
+
+        return current;
+    }
+}
